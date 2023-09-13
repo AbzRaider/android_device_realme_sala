36f880fd671fc0284b77cad5283efb7ffe752ce1
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2023-09-13 07:19:06 +0000

    sala: Initial skeleton

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_sala.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_sala-user \
+    lineage_sala-userdebug \
+    lineage_sala-eng
